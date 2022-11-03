c56b13349983b98ed5c39e335d6465d4b9998227
Sergey Chuguy <mr.chuguy@gmail.com>
2022-11-03 18:13:01 +0200

    Add start files

---
+++ src/js/settings.js
@@ -0,0 +1,6 @@
+const settings = {
+  API_KEY: '8510e0ad31b6bb4a75e3ab4ea305febb',
+  BASE_URL: 'https://api.themoviedb.org/3',
+  BASE_IMG_URL: 'https://image.tmdb.org/t/p/w500',
+};
+export default settings;
