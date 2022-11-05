7374206f18a8f4e5c5f73453af7246b31a73629b
VitalijO <onyshko.vitalij@gmail.com>
2022-11-05 17:47:59 +0200

    commit
    add new func and add export

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,11 +1,11 @@
-const WATCHED = "watched"
-const QUEUE = "queue"
+export const WATCHED = "watched"
+export const QUEUE = "queue"
 
 
 //у функцію передаємо першим парметром - ID фільму, 
 //другим - ОБЄКТ сам фільм, 
-//третім - константу потрібного списку
-function saveMovie(key, value, constName) {
+//третім - константу потрібного списку або watched або queue
+export function saveMovie(key, value, constName) {
   try {
     value.list=constName
     const serializedState = JSON.stringify(value);
@@ -15,20 +15,20 @@ function saveMovie(key, value, constName) {
   }
 };
 
-function loadMovie (key){
+export function loadMovie (key){
   try {
     const serializedState = localStorage.getItem(key);
-    console.log(typeof(serializedState))
     return serializedState === null ? undefined : JSON.parse(serializedState);
   } catch (error) {
     console.error("Get state error: ", error.message);
   }
 };
 
-//у функцію передаємо - константу потрібного списку
-// ретурн відфільтрований масив об`єктів переглянутих або в черзі
+//у функцію передаємо - константу потрібного списку або WATCHED  або QUEUE
+// ретурн відфільтрований масив об`єктів, в залежності, що передали аргументом
+//в constName, це або переглянуті або в фільми черзі
 
-function loadAllMovie(constName) {
+export function loadAllMovie() {
  let arrayMovie = []
   try {
    for(let key in localStorage) 
@@ -43,6 +43,31 @@ function loadAllMovie(constName) {
      }
    }
 return arrayMovie  
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
+
+// функція перевіряє за ID фільму наявність або відсутність фільму в локал сторедж
+// функція повертає 3 варіанти відповіді
+// варіант 1 повертає фолс коли фільму немає в watched і немає в queue
+// варіант 2 повертає watched  - фільм з таким ID знайдений у списку watched
+// варіант 3 повертає queue
+
+export function checkMovieInStorage(ID){
+  try {
+    let result = false
+   for(let key in localStorage) 
+   {
+     if (!localStorage.hasOwnProperty(key)) 
+     { continue; }
+     let serializedState = localStorage.getItem(key);
+     serializedState = JSON.parse(serializedState);
+     if (serializedState.id === ID) {
+      result = serializedState.list
+     }
+   }
+ return result
   } catch (error) {
     console.error("Get state error: ", error.message);
   }
