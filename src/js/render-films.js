94e86d44535c9d62f4d52fd8829dbf06d90afb32
AnastasiiaSteblyna <zanegina93@gmail.com>
2022-11-07 00:14:36 +0200

    +
    забула

--- src/js/render-films.js
+++ src/js/render-films.js
@@ -44,22 +44,30 @@ export function renderFilmsOnLibraryPage(films) {
   galleryContainerLibrary.innerHTML = '';
   const markup = films
     .map(
-      film =>
-        `<div class="photo-card">
-      <a class="photo" href=${film.backdrop_path} >
-      <img  src=https://image.tmdb.org/t/p/w500${film.poster_path} alt="${
-          film.title
-        }" loading="lazy" title="${film.title}"/></a>
-      <div class="info">
-      <p class="info-name">
-      ${film.title}</p>
-      <p class="info-genre-year">${film.genre_ids} | ${Number.parseInt(
-          film.release_date
-        )} <span class="info-rating">${film.vote_average.toFixed(1)}
+      `<li class="gallery__item" data-id="${film.id}">
+              <a class="gallery__link" href="">
+                <div class="gallery__link--wrap">
+                <div class="gallery__img-background">
+                  <img src="https://image.tmdb.org/t/p/w500${nullOrNot(
+                    film.poster_path
+                  )}" alt="${film.title}" loading="lazy"  />
+                 
+                  <img class="gallery__overlay" src="https://image.tmdb.org/t/p/w500${nullOrNot(
+                    film.poster_path
+                  )}" alt="${film.title}" loading="lazy"  />
+                </div>
+                </div>
+                <div class="info">
+                  <p class="info-name"> ${film.title}</p>
+                  <p class="info-genre-year">${renderGenres(
+                    film.genre_ids
+                  )} | ${Number.parseInt(
+        film.release_date
+      )} <span class="info-rating">${film.vote_average.toFixed(1)}
       </span></p>
-
-      </div>
-        </div>`
+                </div>
+              </a>
+            </li>`
     )
     .join('');
 
